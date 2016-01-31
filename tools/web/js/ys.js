110943b64e06dc5d42486bbfca60414d21ed51b4
Sean Middleditch <sean@seanmiddleditch.com>
2016-01-31 15:42:13 -0800

    Begin splitting up the Ys web tool code.

--- tools/web/js/ys.js
+++ tools/web/js/ys.js
@@ -1,93 +1,109 @@
 (function(){
-	window.Ys = {};
+	window.Ys = function(){
+		this.events = {};
+		this.stats = {
+			events: 0,
+			frames: 0,
+			bytes: 0
+		};
+	};
 	
-	function connect(port){
-		var statsDiv = document.getElementById('stats');
-		var countersDiv = document.getElementById('counters');
-		var timersDiv = document.getElementById('timers');
-
-		var stats = { events: 0, blocks: 0, bytes: 0 }
-		var counters = {}
-		var timers = {}
-
-		var lastTick = 0;
-
-		function writeTable(stats, block){
-			var html = '';
-			for (k in stats)
-				html += '<div><span>' + k + '</span><span> = </span><span>' + stats[k] + '</span></div>';
-			block.innerHTML = html;
+	var ys = window.Ys.prototype;
+	
+	ys.on = function(ev, cb){
+		if (ev in this.events)
+			this.events[ev].append(cb);
+		else
+			this.events[ev] = [cb];
+	};
+	
+	ys.emit = function(ev, data){
+		if (ev in this.events)
+		{
+			cbs = this.events[ev];
+			for (var i = 0; i != cbs.length; ++i)
+				cbs[i](data);
 		}
+	};
 
-		DataView.prototype.getUint64 = function(pos, endian){
-			if (endian) {
-				var lo = this.getUint32(pos, endian);
-				var hi = this.getUint32(pos + 4, endian);
-				return hi * Math.pow(2, 32) + lo;
-			} else {
-				var hi = this.getUint32(pos, endian);
-				var lo = this.getUint32(pos + 4, endian);
-				return hi * Math.pow(2, 32) + lo;
-			}
+	// extend DataView to support reading 64-bit unsigned ints,
+	// cast into native JS numbers (doubles). This isn't going to
+	// work for every U64 one might need, but it works for our
+	// current needs.
+	DataView.prototype.getUint64 = function(pos, endian){
+		if (endian) {
+			var lo = this.getUint32(pos, endian);
+			var hi = this.getUint32(pos + 4, endian);
+			return hi * Math.pow(2, 32) + lo;
+		} else {
+			var hi = this.getUint32(pos, endian);
+			var lo = this.getUint32(pos + 4, endian);
+			return hi * Math.pow(2, 32) + lo;
 		}
-
-		function parseEvent(data, pos){
-			var type = data.getUint8(pos);
-			stats.events += 1;
-			switch (type) {
-				case 1 /*HEADER*/:
-					timers.frequencyNanoseconds = data.getUint64(pos + 1, true);
-					timers.frequencySeconds = 1 / timers.frequencyNanoseconds;
-					console.log('HEADER(freq=', stats.freq, ')');
-					return 9;
-				case 2 /*TICK*/:
-					var now = data.getUint64(pos + 1, true);
-					timers.frameNanoseconds = now - lastTick;
-					lastTick = now;
-					timers.dt = timers.frameNanoseconds * timers.frequencySeconds;
-					return 9;
-				case 3 /*REGION*/:
-					var line = data.getUint32(pos + 1, true);
-					var file = data.getUint32(pos + 5, true);
-					var name = data.getUint32(pos + 9, true);
-					var start = data.getUint64(pos + 13, true);
-					var end = data.getUint64(pos + 21, true);
-					//console.log('REGION(file=', file, ' line=', line, ' name=', name, ' start=', start, ' end=', end, ')');
-					return 29;
-				case 4 /*COUNTER*/:
-					var line = data.getUint32(pos + 1, true);
-					var file = data.getUint32(pos + 5, true);
-					var name = data.getUint32(pos + 9, true);
-					var when = data.getUint64(pos + 13, true);
-					var value = data.getFloat64(pos + 21, true);
-					counters[name] = value;
-					return 29;
-				default /*NONE or Unknown*/:
-					console.log('UNKNOWN(pos=', pos, ' type=', type);
-					return data.byteLength;
-			}
+	}
+	
+	function parseEvent(ys, data, pos){
+		++ys.stats.events;
+		
+		var type = data.getUint8(pos);
+		switch (type) {
+			case 1 /*HEADER*/:
+				var ev = {
+					frequency: data.getUint64(pos + 1, true)
+				};
+				ys.emit('header', ev);
+				return 9;
+			case 2 /*TICK*/:
+				var ev = {
+					when: data.getUint64(pos + 1, true)
+				}
+				ys.emit('tick', ev);
+				return 9;
+			case 3 /*REGION*/:
+				var ev = {
+					line: data.getUint32(pos + 1, true),
+					file: data.getUint32(pos + 5, true),
+					name: data.getUint32(pos + 9, true),
+					start: data.getUint64(pos + 13, true),
+					end: data.getUint64(pos + 21, true)
+				};
+				ys.emit('region', ev);
+				return 29;
+			case 4 /*COUNTER*/:
+				var ev = {
+					line: data.getUint32(pos + 1, true),
+					file: data.getUint32(pos + 5, true),
+					name: data.getUint32(pos + 9, true),
+					when: data.getUint64(pos + 13, true),
+					value: data.getFloat64(pos + 21, true)
+				};
+				ys.emit('counter', ev);
+				return 29;
+			default /*NONE or Unknown*/:
+				console.log('UNKNOWN(pos=', pos, ' type=', type, ')');
+				return data.byteLength;
 		}
+	}
 
-		function onMessage(data){
-			var data = new DataView(data);
-			stats.blocks += 1;
-			stats.bytes += data.byteLength;
-			var pos = 0;
-			while (pos < data.byteLength)
-				pos += parseEvent(data, pos);
-			writeTable(stats, statsDiv);
-			writeTable(counters, countersDiv);
-			writeTable(timers, timersDiv);
-		}
-
+	function onMessage(ys, data){
+		++ys.stats.frames;
+		ys.stats.bytes += data.byteLength;
+		
+		var data = new DataView(data);
+		var pos = 0;
+		while (pos < data.byteLength)
+			pos += parseEvent(ys, data, pos);
+	}
+	
+	ys.connect = function(port){
+		var self = this;
+		
 		var ws = new WebSocket('ws://localhost:' + port);
 		ws.binaryType = 'arraybuffer';
 
 		ws.onopen = function(){ console.log('opened'); };
 		ws.onerror = function(error){ console.log('error:', error); };
 		ws.onclosed = function(){ console.log('closed'); };
-		ws.onmessage = function(ev){ onMessage(ev.data); };
-	}
-	
-	Ys.connect = connect;
+		ws.onmessage = function(ev){ onMessage(self, ev.data); };
+	};
 })();
---
+++ tools/web/js/ysView.js
@@ -0,0 +1,38 @@
+(function(){
+	window.YsView = function(ys){
+		var statsDiv = document.getElementById('stats');
+		var countersDiv = document.getElementById('counters');
+		var timersDiv = document.getElementById('timers');
+
+		var counters = {}
+		var timers = {}
+
+		var lastTick = 0;
+
+		function writeTable(stats, block){
+			var html = '';
+			for (k in stats)
+				html += '<div><span>' + k + '</span><span> = </span><span>' + stats[k] + '</span></div>';
+			block.innerHTML = html;
+		}
+		
+		ys.on('header', function(ev){
+			timers.frequencyNS = ev.frequency;
+			timers.frequency = 1 / ev.frequency;
+		});
+
+		ys.on('tick', function(ev){
+			timers.frameNS = ev.when - lastTick;
+			lastTick = ev.when;
+			timers.dt = timers.frameNS * timers.frequency;
+			
+			writeTable(ys.stats, statsDiv);
+			writeTable(timers, timersDiv);
+			writeTable(counters, countersDiv);
+		});
+		
+		ys.on('counter', function(ev){
+			counters[ev.name] = ev.value;
+		});
+	};
+})();
